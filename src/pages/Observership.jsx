b0c751e267b08c2cc93e640bc8d7b8b378a995d9
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-06-03 18:18:19 +0530

    remove jk in observership page

--- src/pages/Observership.jsx
+++ src/pages/Observership.jsx
@@ -50,7 +50,7 @@ const Observership = () => {
 
       <PageTitle title="Observership" />
 
-      {/* Header Banner - Standardized to Team Stylejk */}
+      {/* Header Banner - Standardized to Team Style */}
       <div className="relative py-16 sm:py-24 px-6 text-center overflow-hidden">
         {/* Background Layer */}
         <div className="absolute inset-0 z-0">
