1810cff684e3306c9519df665a1eaded31286d12
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-06-10 17:56:51 +0530

    remove yy in process page

--- src/pages/Process.jsx
+++ src/pages/Process.jsx
@@ -25,7 +25,7 @@ const organImages = [
   { img: kidneyImg, title: 'Nephrology', baseScale: 0.8 }
 ];
 
-// Stethoscope tube coloryy
+// Stethoscope tube color
 const TUBE_COLOR = '#7e2726';
 
 const Process = () => {
