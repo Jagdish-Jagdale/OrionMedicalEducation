e448e5ade4756e1957dcfc68de87459134ea39d9
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-06-13 18:15:47 +0530

    add kk in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -128,7 +128,7 @@ const Contact = () => {
       const { name, value } = e.target;
 
       if (name === 'fullName') {
-         // Only alphabetic characters and spaces
+         // Only alphabetic characters and spaceskk
          const sanitizedValue = value.replace(/[^a-zA-Z\s]/g, '');
          setFormData(prev => ({ ...prev, [name]: sanitizedValue }));
       } else if (name === 'phoneNumber') {
