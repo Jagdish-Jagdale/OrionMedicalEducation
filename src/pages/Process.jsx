f0fc5fd561d408a2050c8bda8b4214b2096b84ba
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-06-26 14:34:37 +0530

    add aj in process page

--- src/pages/Process.jsx
+++ src/pages/Process.jsx
@@ -25,7 +25,7 @@ const organImages = [
   { img: kidneyImg, title: 'Nephrology', baseScale: 0.8 }
 ];
 
-// Stethoscope tube color
+// Stethoscope tube coloraj
 const TUBE_COLOR = '#7e2726';
 
 const Process = () => {
